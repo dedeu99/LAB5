7929ec353e55544bac2d5c3d25d885937e062c0c
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-11 16:35:46 +0100

    trying autofill for register.html

--- register.php
+++ register.php
@@ -1,10 +1,34 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
+require_once "HTML/Template/IT.php";
+
+
+$name="nome1";
+$email="email1";
+
+// Cria um novo objecto template
+ $template = new HTML_Template_IT('.'); 
+
+
+ // Carrega o template Filmes2_TemplateIT.tpl
+ $template->loadTemplatefile('index_template.html',true, true); 
 
 
 
+$template->setCurrentBlock("POSTS");
+$template->setVariable('NAME', $name );
+$template->setVariable('EMAIL', $email);
 
 
-$response = http_get("/", "asd"), $info);
+
+$template->parseCurrentBlock();
+
+
+
+$template->show();
+
+
+$response = http_get("./", "asd"), $info);
 echo "<script type='text/javascript'>alert('$info / &response');</script>";
 
 //header("Location:register.php?error=1&name=Joao%20Silva");
