2bca07787fbf260e7d8c8eade65585eeed17d49b
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-11 16:43:59 +0100

    trying autofill for register.html

--- register.php
+++ register.php
@@ -1,32 +1,32 @@
 <?php
 echo "<script type='text/javascript'>alert('1');</script>";
-header('Content-Type: text/html; charset=utf-8');
-require_once "HTML/Template/IT.php";
+//header('Content-Type: text/html; charset=utf-8');
+//require_once "HTML/Template/IT.php";
 echo "<script type='text/javascript'>alert('2');</script>";
 
-$name="nome1";
-$email="email1";
+//$name="nome1";
+//$email="email1";
 echo "<script type='text/javascript'>alert(3');</script>";
 // Cria um novo objecto template
-template = new HTML_Template_IT('.'); 
+//template = new HTML_Template_IT('.'); 
 
 echo "<script type='text/javascript'>alert('4');</script>";
-$template->loadTemplatefile('register_template.html',true, true); 
+//$template->loadTemplatefile('register_template.html',true, true); 
 
 
 echo "<script type='text/javascript'>alert('5');</script>";
-$template->setCurrentBlock("REGISTERFORM");
+//$template->setCurrentBlock("REGISTERFORM");
 echo "<script type='text/javascript'>alert('6');</script>";
-$template->setVariable('NAME', $name );
-$template->setVariable('EMAIL', $email);
+//$template->setVariable('NAME', $name );
+//$template->setVariable('EMAIL', $email);
 
 echo "<script type='text/javascript'>alert('7');</script>";
 
-$template->parseCurrentBlock();
+//$template->parseCurrentBlock();
 
 echo "<script type='text/javascript'>alert('8');</script>";
 
-$template->show();
+//$template->show();
 
 
 //$response = http_get("./", "asd"), $info);
