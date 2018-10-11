b1697eb60562c7b0f78fd5515db48759dce94349
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-11 16:27:22 +0100

    starting trying autofill for register.html

--- register.php
+++ register.php
@@ -1,7 +1,13 @@
 <?php
 
-echo "<script type='text/javascript'>alert('$FORM IS WORKING');</script>";
 
 
+
+
+$response = http_get("/", "asd"), $info);
+echo "<script type='text/javascript'>alert('$info / &response');</script>";
+
+//header("Location:register.php?error=1&name=Joao%20Silva");
+//header("Location: register_success.html"); 
 
 ?>
