14355ab36f76276736f54477014a526947679920
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-15 10:46:17 +0100

    adding funcionality

--- register.php
+++ register.php
@@ -2,39 +2,39 @@
 echo "<script type='text/javascript'>alert('1');</script>";
 header('Content-Type: text/html; charset=utf-8');
 require_once "HTML/Template/IT.php";
-echo "<script type='text/javascript'>alert('2');</script>";
+
 
 $name=$_GET['name'];
 $email=$_GET['email'];
 $message=$_GET['ERROR'];
 //{MESSAGE_HIDDEN}
-echo "<script type='text/javascript'>alert('3');</script>";
+
 // Cria um novo objecto template
 $template = new HTML_Template_IT('.'); 
 
-echo "<script type='text/javascript'>alert('4');</script>";
+
 $template->loadTemplatefile('register_template.html',true, true); 
 
 
-echo "<script type='text/javascript'>alert('5');</script>";
+
 $template->setCurrentBlock("REGISTERFORM");
-echo "<script type='text/javascript'>alert('6');</script>";
+
 $template->setVariable('NAME', $name );
 $template->setVariable('EMAIL', $email);
 $template->setVariable('MESSAGE', $message);
 $template->setVariable('MESSAGE_HIDDEN',"hidden");
 
-echo "<script type='text/javascript'>alert('7');</script>";
+
 
 $template->parseCurrentBlock();
 
-echo "<script type='text/javascript'>alert('8');</script>";
+
 
 $template->show();
 
 
 //$response = http_get("./", "asd"), $info);
-echo "<script type='text/javascript'>alert('9');</script>";
+
 
 //header("Location:register.php?error=1&name=Joao%20Silva");
 //header("Location: register_success.html"); 
--- register_action.php
+++ register_action.php
@@ -1,8 +1,21 @@
 <?php
 
-
+include 'db.php';
 $name=$_POST['name'];
 $email=$_POST['email'];
+
+
+// ligação à base de dados
+$db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
+// criar query numa string
+$query = "SELECT email FROM users WHERE email=".$email;
+
+echo "<script type='text/javascript'>alert('SELECT count(*) FROM users WHERE email=".$email."');</script>";
+// executar a query
+$result = @ mysql_query($query,$db ); 
+$nrows = mysql_num_rows($result); 
+echo "<script type='text/javascript'>alert('".$nrows."');</script>";
+
 
 header("Location:register.php?error=1&name=".$name."&email=".$email);
 
