8669a0f943030f404e9f83a45e8c4192cf04dd81
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-15 10:51:40 +0100

    adding funcionality

--- register_action.php
+++ register_action.php
@@ -13,6 +13,7 @@ $query = "SELECT email FROM users WHERE email=".$email;
 echo "<script type='text/javascript'>alert('SELECT count(*) FROM users WHERE email=\'".$email."\'');</script>";
 // executar a query
 $result = @ mysql_query($query,$db ); 
+var_dump($result);
 $nrows = mysql_num_rows($result); 
 echo "<script type='text/javascript'>alert('".$nrows."');</script>";
 
