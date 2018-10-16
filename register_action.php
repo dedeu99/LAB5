7250be2ecb61f4d75efb8833a6d3e4272cccc80a
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-16 10:25:28 +0100

    adding funcionality

--- register_action.php
+++ register_action.php
@@ -3,6 +3,8 @@
 include 'db.php';
 $name=$_POST['name'];
 $email=$_POST['email'];
+$password=$_POST['password'];
+$password2=$_POST['passwordConfirmation'];
 
 
 // ligação à base de dados
@@ -10,13 +12,26 @@ $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 // criar query numa string
 $query = "SELECT email FROM users WHERE email='".$email."'";
 
-echo "<script type='text/javascript'>alert('SELECT count(*) FROM users WHERE email=\'".$email."\'');</script>";
+//echo "<script type='text/javascript'>alert('SELECT count(*) FROM users WHERE email=\'".$email."\'');</script>";
 // executar a query
 $result = @ mysql_query($query,$db ); 
 var_dump($result);
 $nrows = mysql_num_rows($result); 
-echo "<script type='text/javascript'>alert('".$nrows."');</script>";
+//echo "<script type='text/javascript'>alert('".$nrows."');</script>";
 
+if(password!=password2)
+	header("Location:register.php?error=2&name=".$name."&email=".$email."");
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+	header("Location:register.php?error=1&name=".$name."&email=".$email."");	        
+
+if(nrows==0)
+{
+	$query = "INSERT INTO users (name,email,created_at,updated_at,password_digest,remember_digest,admin) VALUES ('".$name."','".$email."',NOW(),NOW(),'".$password."','".$password2."',1);"	
+	header("Location: register_success.html"); 
+}else
+{
+	header("Location:register.php?error=3&name=".$name."&email=".$email."");
+}
 
 //header("Location:register.php?error=1&name=".$name."&email=".$email);
 
