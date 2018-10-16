7ba1caf86c80e18ce0ae8604e8ecf10aee849308
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-16 15:18:23 +0100

    adding funcionality

--- register_action.php
+++ register_action.php
@@ -7,6 +7,29 @@ $password=$_POST['password'];
 $password2=$_POST['passwordConfirmation'];
 
 
+//echo "<script type='text/javascript'>alert('\'".$password."\'');</script>";
+//echo "<script type='text/javascript'>alert('1');</script>";
+echo "<script type='text/javascript'>alert('password:".$password.":');</script>";
+echo "<script type='text/javascript'>alert('password2:".$password2.":');</script>";
+echo "<script type='text/javascript'>alert('email:".$email.":');</script>";
+echo "<script type='text/javascript'>alert('empty(password):".empty($password).":');</script>";
+echo "<script type='text/javascript'>alert('empty(password2):".empty($password2).":');</script>";
+echo "<script type='text/javascript'>alert('strcmp(password,password2):".strcmp($password,$password2).":');</script>";
+
+if(empty($name)) {
+	header("Location:register.php?error=1&email=".$email."");//ERROR1 BAD NAME
+}
+if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	header("Location:register.php?error=2&name=".$name."&email=".$email."");//ERROR2 BAD EMAIL
+}
+if(empty($password) || empty($password2) || strcmp($password,$password2)!=0)
+	header("Location:register.php?error=3&name=".$name."&email=".$email."");//ERROR3 BAD PASSWORDCONFIRMATION
+
+	//header("Location: register_success.html"); //TO REMOVE
+
+
+
+
 // ligação à base de dados
 $db = dbconnect($hostname,$db_name,$db_user,$db_passwd);
 // criar query numa string
@@ -17,26 +40,7 @@ $query = "SELECT email FROM users WHERE email='".$email."'";
 $result = @ mysql_query($query,$db ); 
 //var_dump($result);
 $nrows = mysql_num_rows($result); 
-//echo "<script type='text/javascript'>alert('\'".$password."\'');</script>";
-//echo "<script type='text/javascript'>alert('1');</script>";
-echo "<script type='text/javascript'>alert('password:".$password.":');</script>";
-echo "<script type='text/javascript'>alert('password2:".$password2.":');</script>";
-echo "<script type='text/javascript'>alert('email:".$email.":');</script>";
-echo "<script type='text/javascript'>alert('empty(password):".empty($password).":');</script>";
-echo "<script type='text/javascript'>alert('empty(password2):".empty($password2).":');</script>";
-echo "<script type='text/javascript'>alert('strcmp(password,password2):".strcmp($password,$password2).":');</script>";
 
-if(empty($password) || empty($password2) || strcmp($password,$password2)!=0)
-	echo "ERROR 2";//header("Location:register.php?error=2&name=".$name."&email=".$email."");
-if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) 
-	echo "ERROR 1";//header("Location:register.php?error=1&name=".$name."&email=".$email."");
-
-
-	//header("Location: register_success.html"); //TO REMOVE
-
-
-
-/*
 if(nrows==0)
 {
 	$query = "INSERT INTO users (name,email,created_at,updated_at,password_digest,remember_digest,admin) VALUES ('".$name."','".$email."',NOW(),NOW(),'".$password."','".$password2."',1);"	
@@ -47,11 +51,11 @@ if(nrows==0)
 		header("Location: register_success.html"); 
 	else
 	{
-		header("Location:register.php?error=4&name=".$name."&email=".$email);
+		header("Location:register.php?error=4&name=".$name."&email=".$email);//ERROR4 COULDN'T UPDATE THE DATABASE TRY AGAIN
 	}
 }else
 {
-	header("Location:register.php?error=3&name=".$name."&email=".$email);
+	header("Location:register.php?error=5&name=".$name."&email=".$email);//ERROR5 EMAIL ALREADY IN USE
 }
 */
 //header("Location:register.php?error=1&name=".$name."&email=".$email);
