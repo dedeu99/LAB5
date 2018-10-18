a3130eb1a9eb0b47a2dace7c94eaadd9e9ee6e3c
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-18 15:56:02 +0100

    adding funcionality

--- register_success.php
+++ register_success.php
@@ -5,6 +5,6 @@ header('Refresh: 5; url='.base_url());
 $referrer = $_SERVER['HTTP_REFERER']; 
 echo base_url();
 echo "http://all.deei.fct.ualg.pt/~a62362/LAB5/";
-if(strcmp($referrer,))
+//if(strcmp($referrer,))
 	echo "<script type='text/javascript'>alert('referrer:$referrer.');</script>";
 ?>
