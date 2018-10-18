1176ff4476012a644dfd8259560cd672e2253266
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-18 16:29:32 +0100

    adding funcionality

--- register_success.php
+++ register_success.php
@@ -2,15 +2,10 @@
 
 header('Refresh: 5; url=index.php');
 
-$referrer = $_SERVER['HTTP_REFERER']; 
-$base=$_SERVER['DOCUMENT_ROOT'];
+$referer = $_SERVER['HTTP_REFERER']; 
+$domain = parse_url($referer);
 
-echo $referrer;
+echo $referer;
 echo "\n-----\n";
-$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http")."://$_SERVER[HTTP_HOST]"."register.php";
-echo $actual_link;
-//echo base_url();
-//echo "http://all.deei.fct.ualg.pt/~a62362/LAB5/";
-//if(strcmp($referrer,))
-//	echo "<script type='text/javascript'>alert('referrer:$referrer.');</script>";
+echo basename($domain);
 ?>
