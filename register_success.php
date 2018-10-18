58deb1c8b9316f61322b87715e1f5cb8c35c7215
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-18 16:16:15 +0100

    adding funcionality

--- register_success.php
+++ register_success.php
@@ -7,7 +7,8 @@ $base=$_SERVER['DOCUMENT_ROOT'];
 
 echo $referrer;
 echo "\n-----\n";
-echo __FILE__;
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http")."://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+echo $actual_link;
 //echo base_url();
 //echo "http://all.deei.fct.ualg.pt/~a62362/LAB5/";
 //if(strcmp($referrer,))
