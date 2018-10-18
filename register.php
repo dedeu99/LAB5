4e2494ef9af0b5b262d3e84816fcb4096b0578a4
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-18 16:46:19 +0100

    adding funcionality

--- register.php
+++ register.php
@@ -6,7 +6,7 @@ require_once "HTML/Template/IT.php";
 
 $name=$_GET['name'];
 $email=$_GET['email'];
-$message=$_GET['ERROR'];
+$message=$_GET['error'];
 //{MESSAGE_HIDDEN}
 
 // Cria um novo objecto template
@@ -17,15 +17,38 @@ $template->loadTemplatefile('register_template.html',true, true);
 
 
 
+
 $template->setCurrentBlock("REGISTERFORM");
+
+
+
+
 
 $template->setVariable('NAME', $name );
 $template->setVariable('EMAIL', $email);
+
+//$template->setVariable('MESSAGE_HIDDEN',"hidden");
+
+switch($message){
+	case "1":
+		$message="Please fill in your name";
+	break;
+	case "2":
+		$message="Please insert a valid email";
+	break;
+	case "3":
+		$message="Please make sure your password is filled and the confirmation matches";
+	break;
+	case "4":
+		$message="Database error, couldn't create a new user. Please try again later";
+	break;
+	case "5":
+		$message="Email is already in use. Please check the email used and, if it really is yours, recover your password";
+	break;
+	default:
+	template->setVariable('MESSAGE_HIDDEN',"hidden");
+}
 $template->setVariable('MESSAGE', $message);
-$template->setVariable('MESSAGE_HIDDEN',"hidden");
-
-
-
 $template->parseCurrentBlock();
 
 
--- register_success.php
+++ register_success.php
@@ -12,7 +12,7 @@ print_r($domain);
 echo PHP_EOL.basename($domain[path]).PHP_EOL*/
 
 if(strcmp(basename($domain[path]),"register.php")==0)
-	echo "SUCESSFULLY CREATED";
+	echo "SUCCESSFULLY CREATED";
 else
 	echo "how did you get here again?";
 
