044a8db57125ac75df33fef4931b46cf3fc2f489
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-25 16:06:55 +0100

    m

--- register_success.php
+++ register_success.php
@@ -2,8 +2,8 @@
 
 header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
+header("Cache-Control: private, no-store, max-age=0, no-cache, must-revalidate, post-check=0, pre-check=0");
+//header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header("Connection: close");
 header('Content-Type: text/html; charset=utf-8');
