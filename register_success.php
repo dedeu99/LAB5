016458f19ce27e684c3439c7b1e3446c36c8315c
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-25 15:58:41 +0100

    m

--- register_success.php
+++ register_success.php
@@ -2,6 +2,11 @@
 
 header('Content-Type: text/html; charset=utf-8');
 header('Refresh: 5; url=index.php');
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
+
 require_once "HTML/Template/IT.php";
 
 
